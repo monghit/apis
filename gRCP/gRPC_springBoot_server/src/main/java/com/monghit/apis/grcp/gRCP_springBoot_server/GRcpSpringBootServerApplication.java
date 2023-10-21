b53f0a8d18b3597929bc1e749f5f97a7e355e5f6
josemaria <josemaria.gonzalez@entelgydigital.com>
2023-10-21 13:20:43 +0200

    first commit

---
+++ gRCP/gRPC_springBoot_server/src/main/java/com/monghit/apis/grcp/gRCP_springBoot_server/GRcpSpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package com.monghit.apis.grcp.gRCP_springBoot_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRcpSpringBootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GRcpSpringBootServerApplication.class, args);
+	}
+
+}
---
+++ gRCP/gRPC_springBoot_server/src/test/java/com/monghit/apis/grcp/gRCP_springBoot_server/GRcpSpringBootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monghit.apis.grcp.gRCP_springBoot_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRcpSpringBootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
